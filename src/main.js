e2f74aec121c1dd9e3bbc646519faf7f51591cce
Robin Millette <robin@millette.info>
2016-07-17 08:21:43 -0400

    Use Array.from on nodelists.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 (function () {
-  var debugPing = true
+  'use strict'
+  var debugPing = false
 
   var ping = debugPing
     ? function (a) { console.log(new Date(), a) }
@@ -7,13 +8,12 @@
 
   // For discussion and comments, see: http://remysharp.com/2009/01/07/html5-enabling-script/
   var addEvent = (function () {
-    var i
     if (document.addEventListener) {
       return function (el, type, fn) {
         if (el && el.nodeName || el === window) {
           el.addEventListener(type, fn, false)
         } else if (el && el.length) {
-          for (i = 0; i < el.length; ++i) { addEvent(el[i], type, fn) }
+          Array.from(el).forEach(function (elem) { addEvent(elem, type, fn) })
         }
       }
     } else {
@@ -21,7 +21,7 @@
         if (el && el.nodeName || el === window) {
           el.attachEvent('on' + type, function () { return fn.call(el, window.event) })
         } else if (el && el.length) {
-          for (i = 0; i < el.length; ++i) { addEvent(el[i], type, fn) }
+          Array.from(el).forEach(function (elem) { addEvent(elem, type, fn) })
         }
       }
     }
@@ -30,32 +30,24 @@
   var bin = document.querySelector('#bin')
   var draggedOver = false
 
-  var dragStart = function (ev) {
-    ping('dragstart')
-    ping(this.id)
-    ev.dataTransfer.effectAllowed = 'copy' // only dropEffect='copy' will be dropable
-    ev.dataTransfer.setData('Text', this.id) // required otherwise doesn't work
-  }
-
-  var dragEnd = function (ev) {
-    ping('dragend')
-    ping(ev)
-    ping(this)
-  }
-
   var setDraggables = function () {
-    var i
-    var elem
     var rnd = Math.floor(Math.random() * 1e6)
     var links = document.querySelectorAll('li')
 
-    for (i = 0; i < links.length; ++i) {
-      elem = links[i]
+    var dragStart = function (ev) {
+      ping('dragstart:' + this.id)
+      ev.dataTransfer.effectAllowed = 'copy' // only dropEffect='copy' will be dropable
+      ev.dataTransfer.setData('Text', this.id) // required otherwise doesn't work
+    }
+
+    var dragEnd = function () { ping('dragend') }
+
+    Array.from(links).forEach(function (elem, i) {
       elem.setAttribute('draggable', 'true')
       if (!elem.id) { elem.id = ('dndrnd-' + rnd) + (i + 1) }
       addEvent(elem, 'dragstart', dragStart)
       addEvent(elem, 'dragend', dragEnd)
-    }
+    })
   }
 
   setDraggables()
@@ -65,7 +57,9 @@
       ping('dragover')
       draggedOver = true
     }
-    if (e.preventDefault) { e.preventDefault() } // allows us to drop
+
+    // allows us to drop
+    if (e.preventDefault) { e.preventDefault() }
     e.dataTransfer.dropEffect = 'copy'
     return false
   })
@@ -89,7 +83,6 @@
     var el = document.getElementById(e.dataTransfer.getData('Text'))
     var yum = document.createElement('p')
     yum.style.opacity = 1
-
     ping('drop')
     draggedOver = false
 
@@ -98,7 +91,7 @@
     if (e.stopPropagation) { e.stopPropagation() }
 
     el.parentNode.removeChild(el)
-    // stupid nom text + fade effect
+    // stupid yum text + fade effect
     this.classList.remove('over')
     yum.innerHTML = eat[parseInt(Math.random() * eat.length)]
     y = yum.cloneNode(true)

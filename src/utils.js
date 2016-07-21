50a4a0f8c3b7015379d7ae78af5f1bd9b0664033
Robin Millette <robin@millette.info>
2016-07-21 16:13:35 -0400

    Readding original drag and drop code to move issues between milestones.

--- src/utils.js
+++ src/utils.js
@@ -2,10 +2,132 @@
 
 // npm
 var pagejs = require('page')
+var groupBy = require('lodash.groupby')
 var $ = require('jquery')
 $.fn.render = require('transparency').jQueryPlugin
 
+var debugPing = true
+
+var ping = debugPing
+  ? function (a) { console.log(new Date(), a) }
+  : function () { }
+
 var appData
+
+var addEvent = (function () {
+  'use strict'
+
+  if (document.addEventListener) {
+    return function (el, type, fn) {
+      if (el && el.nodeName || el === window) {
+        el.addEventListener(type, fn, false)
+      } else if (el && el.length) {
+        Array.from(el).forEach(function (elem) { addEvent(elem, type, fn) })
+      }
+    }
+  } else {
+    return function (el, type, fn) {
+      if (el && el.nodeName || el === window) {
+        el.attachEvent('on' + type, function () { return fn.call(el, window.event) })
+      } else if (el && el.length) {
+        Array.from(el).forEach(function (elem) { addEvent(elem, type, fn) })
+      }
+    }
+  }
+})()
+
+var zzz = function (issuesData) {
+  var setDraggables = function () {
+    var links = document.querySelectorAll('ul.issues li')
+
+    var dragStart = function (ev) {
+      ping('dragstart:' + this.id)
+      ev.dataTransfer.effectAllowed = 'copy' // only dropEffect='copy' will be dropable
+      ev.dataTransfer.setData('Text', this.id) // required otherwise doesn't work
+    }
+
+    var dragEnd = function () { ping('dragend') }
+
+    Array.from(links).forEach(function (elem, i) {
+      elem.setAttribute('draggable', 'true')
+      addEvent(elem, 'dragstart', dragStart)
+      addEvent(elem, 'dragend', dragEnd)
+    })
+  }
+
+  var body = document.querySelector('#repositorypage')
+  var draggedOver = false
+  var byMilestone = groupBy(issuesData, function (x) {
+    return x.milestone && x.milestone.number || 'none'
+  })
+
+  console.log('byMilestone:', byMilestone)
+
+  Object.keys(byMilestone).forEach(function (milestoneNumber) {
+    // console.log('ML:', milestoneNumber)
+    var div = document.createElement('div')
+    var h2 = document.createElement('h2')
+    var zone = document.createElement('ul')
+    var hasTitle = byMilestone[milestoneNumber][0].milestone
+    zone.className = 'issues'
+    h2.innerHTML = hasTitle ? hasTitle.title : 'Sans milestone'
+    zone.id = 'milestone-' + milestoneNumber
+    byMilestone[milestoneNumber].forEach(function (z) {
+      var el = document.createElement('li')
+      var txt = 'Issue #' + z.number
+      el.id = 'issue-' + z.number
+      z.labels.forEach(function (l) {
+        txt += ' <small class="label" style="color: white; background: #' + l.color + ';">' + l.name + '</small>'
+      })
+      txt += '<br>' + z.title
+      el.innerHTML = txt
+      zone.appendChild(el)
+    })
+    div.appendChild(h2)
+    div.appendChild(zone)
+    body.appendChild(div)
+  })
+  console.log('parsed json', issuesData)
+  setDraggables()
+  var bin = document.querySelectorAll('ul.issues')
+
+  addEvent(bin, 'dragover', function (e) {
+    if (!draggedOver) {
+      ping('dragover')
+      draggedOver = true
+    }
+
+    // allows us to drop
+    if (e.preventDefault) { e.preventDefault() }
+    e.dataTransfer.dropEffect = 'copy'
+    return false
+  })
+
+  addEvent(bin, 'dragenter', function (e) {
+    ping('dragenter')
+    this.classList.add('over')
+    return false
+  })
+
+  addEvent(bin, 'dragleave', function () {
+    ping('dragleave')
+    draggedOver = false
+    this.classList.remove('over')
+  })
+
+  addEvent(bin, 'drop', function (e) {
+    var el = document.getElementById(e.dataTransfer.getData('Text'))
+    ping('drop')
+    draggedOver = false
+    // stops the browser from redirecting...why???
+    e.preventDefault()
+    if (e.stopPropagation) { e.stopPropagation() }
+
+    this.appendChild(el)
+    this.classList.remove('over')
+    return false
+  })
+}
 
 var hideAll = (function () {
   var $divs = $('#app > div')
@@ -56,7 +178,6 @@ var ghFetch = function (u, token, doc) {
 }
 
 var fetchIssues = function (full_name, token, doc) {
-  // https://api.github.com/repos/millette/committed-streaker/issues
   return ghFetch('https://api.github.com/repos/' + full_name + '/issues', token, doc)
 }
 
@@ -73,15 +194,6 @@ var fetchUser = function (token, doc) {
 
 var fetchRepositories = function (token, doc) {
   return ghFetch('https://api.github.com/user/repos', token, doc)
-/*
-    .then(function (j) {
-      var error
-      if (j && j.login) { return j }
-      error = new Error(j.message)
-      error._headers = j._headers
-      throw error
-    })
-*/
 }
 
 var showError = (function () {
@@ -130,8 +242,6 @@ var setupHomeForm = function () {
   })
 }
 
-// var dostuff = function () { return Promise.resolve() }
-
 var repoPage = function (path, title, sel, ctx, next) {
   var found = false
   var full_name = [ctx.params.login, ctx.params.repo].join('/')
@@ -144,18 +254,22 @@ var repoPage = function (path, title, sel, ctx, next) {
         found = true
         if (repository.issues) {
           $(sel).render(repository)
+          zzz(repository.issues)
         } else {
           fetchIssues(full_name, appData.token, repository)
             .then(function (ya) {
               console.log('DOSTUFF', ya)
               repository.issues = ya
               $(sel).render(repository)
+              zzz(repository.issues)
             })
         }
       }
     })
   }
-  if (!found) { return showError('Il n\'y a rien ici.') }
+  if (!found) {
+    showError('Il n\'y a rien ici.')
+  }
 }
 
 var userPage = (function () {
